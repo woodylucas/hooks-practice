93e526314bcf891a2bc5721576c4af5afd08daca
woodylucas <wlucas0311@gmail.com>
2021-07-02 13:27:50 -0400

    feat: integrated debouncing w/ setTimeout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Accordion from "./components/Accordion";
+import Search from "./components/Search";
 
 const items = [
   {
@@ -21,7 +22,8 @@ const items = [
 function App() {
   return (
     <div>
-      <Accordion items={items} />
+      <Search />
+      {/* <Accordion items={items} /> */}
     </div>
   );
 }
